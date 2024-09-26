19ec92f3d5ff10121ca5495784d7dd023b99230f
Gayeon Lee <gayean430@gmail.com>
2024-09-26 22:59:37 +0900

    feature: 그룹 목록 페이지 레이아웃 설계

---
+++ src/components/group/CardList.jsx
@@ -0,0 +1,56 @@
+import empty from "@assets/empty.svg";
+import Button from "@components/all/Button";
+import GroupCard from "@/components/group/GroupCard";
+import LoadMoreButton from "@/components/group/LoadMoreButton";
+import styles from "./CardList.module.css";
+
+const CardList = ({ isPublic, variant, cards }) => {
+  const getEmptyStateText = () => {
+    if (variant === "group" && isPublic)
+      return {
+        mainText: "등록된 공개 그룹이 없습니다.",
+        subText: "가장 먼저 그룹을 만들어보세요!",
+      };
+    else if (variant === "group" && !isPublic)
+      return {
+        mainText: "등록된 비공개 그룹이 없습니다.",
+        subText: "가장 먼저 비공개 그룹을 만들어보세요!",
+      };
+    else if (variant === "memory")
+      return {
+        mainText: "게시된 추억이 없습니다.",
+        subText: "첫 번째 추억을 올려보세요!",
+      };
+  };
+
+  const renderEmptyState = () => {
+    const { mainText, subText } = getEmptyStateText();
+    return (
+      <div className={styles.emptyList}>
+        <div className={styles.emptyImg}>
+          <img src={empty} alt="데이터없음" />
+        </div>
+        <div className={styles.emptyDescription}>
+          <span className="typo-18-bold">{mainText}</span>
+          <span className="typo-14-regular">{subText}</span>
+        </div>
+        <Button>{variant === "group" ? "그룹 만들기" : "추억 올리기"}</Button>
+      </div>
+    );
+  };
+
+  const renderCards = () => (
+    <div className={styles.cardList}>
+      <div className={styles.cardGrid}>
+        {cards.map((card) => (
+          <GroupCard key={card.id}></GroupCard>
+        ))}
+      </div>
+      <LoadMoreButton />
+    </div>
+  );
+
+  return <>{cards.length === 0 ? renderEmptyState() : renderCards()}</>;
+};
+
+export default CardList;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import Header from "@components/all/Header";
-import SearchBar from "../components/main/SearchBar";
-import LoadMoreButton from "../components/main/LoadMoreButton";
-import GroupCard from "../components/main/GroupCard";
-import CardGrid from "../components/main/CardGrid";
+import SearchBar from "@/components/group/SearchBar";
+import CardList from "@/components/group/CardList";
+
+const mockData = [
+  {
+    id: 1,
+    name: "string",
+  },
+  {
+    id: 2,
+    name: "string",
+  },
+  {
+    id: 3,
+    name: "string",
+  },
+  {
+    id: 4,
+    name: "string",
+  },
+  {
+    id: 5,
+    name: "string",
+  },
+];
 
 export default function Main() {
+  const [isPublic, setIsPublic] = useState(true);
   return (
     <>
-      <Header button={true} />
-      <SearchBar />
-      <CardGrid />
+      <Header button />
+      <SearchBar isPublic={isPublic} setIsPublic={setIsPublic} />
+      <CardList isPublic={isPublic} variant="group" cards={[]} />
     </>
   );
 }
