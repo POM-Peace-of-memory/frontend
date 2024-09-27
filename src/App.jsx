2a39d7e53ede00f3cf22de3ea9e24694b87b0e36
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-09-27 19:40:47 +0900

    Feature: 비공개 추억 접근 권한 확인 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "@pages/Main";
 import PostPage from "@pages/Post";
+import PrivatePostPage from "@pages/PrivatePost";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/feed" element={<PostPage />} />
+        <Route path="/private" element={<PrivatePostPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/PrivatePost.jsx
@@ -0,0 +1,11 @@
+import Header from "@components/all/Header";
+import PrivatePost from "@components/feed/PrivatePost";
+
+export default function Main() {
+  return (
+    <>
+      <Header />
+      <PrivatePost />
+    </>
+  );
+}
