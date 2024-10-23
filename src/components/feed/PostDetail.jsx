ab15e6d35083a9b9e9c534e5d19f9bfb74d8b52b
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-10-23 13:50:07 +0900

    Fix: 게시물 상세 페이지

--- src/components/feed/PostDetail.jsx
+++ src/components/feed/PostDetail.jsx
@@ -6,11 +6,20 @@ import DeleteModal from "./DeleteModal";
 import CommentModal from "../chueok/CommentModal";
 import CommentEditModal from "../chueok/CommentEditModal";
 import CommentDeleteModal from "@components/chueok/CommentDeleteModal";
-import { createComment, deleteComment, updateComment } from "@utils/api";
+import flowerImg from "@/assets/flower.svg";
+import commentCount from "@/assets/comment.svg";
+import {
+  getPostDetail,
+  getComment,
+  createComment,
+  deleteComment,
+  updateComment,
+} from "@utils/api";
 
 const PostDetail = () => {
   const { postId } = useParams();
-  console.log(postId);
+
+  const [postDetail, setPostDetail] = useState(null);
   const [comments, setComments] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
@@ -25,27 +34,33 @@ const PostDetail = () => {
   const [showCommentDeleteModal, setShowCommentDeleteModal] = useState(false);
   const [deletingCommentId, setDeletingCommentId] = useState(null);
 
+  const fetchPostDetails = async () => {
+    try {
+      const data = await getPostDetail(postId);
+      setPostDetail(data);
+    } catch (error) {
+      setErrorMessage(error.message);
+      console.error(error);
+    }
+  };
+
   const fetchComments = async (page = 1) => {
     try {
-      const response = await fetch(
-        `/api/posts/${postId}/comments?page=${page}&pageSize=10`
-      );
-      if (!response.ok) {
-        throw new Error("Failed to fetch comments.");
-      }
-      const data = await response.json();
+      const data = await getComment(postId, { page, pageSize: 10 });
+      console.log(data);
       setComments(data.data);
       setCurrentPage(data.currentPage);
       setTotalPages(data.totalPages);
     } catch (error) {
-      console.error(error);
       setErrorMessage("댓글을 불러오는데 실패했습니다.");
+      console.error(error);
     }
   };
 
   useEffect(() => {
+    fetchPostDetails();
     fetchComments(currentPage);
-  }, [currentPage]);
+  }, [postId, currentPage]);
 
   const handleCommentButtonClick = async (event) => {
     event.preventDefault();
@@ -130,130 +145,166 @@ const PostDetail = () => {
 
   return (
     <div className={styles.postDetailContainer}>
-      <div className={styles.writer}>
-        <span>달봉이아들</span>
-        <span className={styles.line}>|</span>
-        <span className={styles.postPrivacy}>공개</span>
-        <button
-          className={styles.edit}
-          onClick={() => setShowModifyModal(true)}
-        >
-          추억 수정하기
-        </button>
-        <button
-          className={styles.delete}
-          onClick={() => setShowDeleteModal(true)}
-        >
-          추억 삭제하기
-        </button>
-      </div>
-
-      <button
-        type="submit"
-        className={styles.commentButton}
-        onClick={handleCommentButtonClick}
-      >
-        댓글 등록하기
-      </button>
-
-      <div className={styles.commentSection}>
-        <h3>댓글 {comments.length}</h3>
-        <form
-          onSubmit={(e) => e.preventDefault()}
-          className={styles.commentForm}
-        >
-          <textarea
-            value={newComment}
-            onChange={(e) => setNewComment(e.target.value)}
-            placeholder="댓글을 입력해 주세요"
-            className={styles.commentInput}
-          />
-        </form>
-      </div>
-
-      {errorMessage && <p className={styles.error}>{errorMessage}</p>}
-
-      <div className={styles.commentList}>
-        {comments.map((comment) => (
-          <div key={comment.id} className={styles.commentItem}>
-            <div className={styles.commentInfo}>
-              <span className={styles.commentAuthor}>{comment.nickname}</span>
-              <span className={styles.commentDate}>
-                {new Date(comment.createdAt).toLocaleString()}
-              </span>
-            </div>
-            <div className={styles.commentContent}>
-              <p className={styles.commentText}>{comment.content}</p>
-              <div className={styles.commentActions}>
-                <button
-                  className={styles.editButton}
-                  onClick={() => handleEditCommentClick(comment)}
-                >
-                  <img src="src/assets/edit.svg" alt="수정" />
-                </button>
-                <button
-                  className={styles.deleteButton}
-                  onClick={() => handleDeleteCommentClick(comment.id)}
-                >
-                  <img src="src/assets/delete.svg" alt="삭제" />
-                </button>
-              </div>
-            </div>
+      {postDetail ? (
+        <>
+          <div className={styles.writer}>
+            <span>{postDetail.nickname}</span>
+            <span className={styles.line}>
+              | {postDetail.isPublic ? "공개" : "비공개"}
+            </span>
           </div>
-        ))}
-      </div>
+          <div className={styles.actions}>
+            <button
+              className={styles.edit}
+              onClick={() => setShowModifyModal(true)}
+            >
+              추억 수정하기
+            </button>
+            <button
+              className={styles.delete}
+              onClick={() => setShowDeleteModal(true)}
+            >
+              추억 삭제하기
+            </button>
+          </div>
+          <div className={styles.titleSection}>
+            <h1>{postDetail.title}</h1>
+            <span>{postDetail.tags}</span>
+          </div>
+          <div className={styles.postInfo}>
+            <span>
+              {postDetail.location} * {postDetail.createdAt}
+            </span>
+            <span className={styles.line}>| </span>
+            <span className={styles.views}>
+              <img src={flowerImg} alt="like" /> {postDetail.likeCount}
+            </span>
+            <span className={styles.commentCount}>
+              <img src={commentCount} alt="comment" /> {postDetail.commentCount}
+            </span>
+          </div>
 
-      <div className={styles.pagination}>
-        <button
-          disabled={currentPage === 1}
-          onClick={() => handlePaginationClick(currentPage - 1)}
-        >
-          &laquo;
-        </button>
-        {Array.from({ length: totalPages }, (_, i) => (
+          <div className={styles.imageSection}>
+            {postDetail.imageUrl === "" ? (
+              <div className={styles.noneImg}>
+                <img src={flowerImg} alt="게시물이미지" />
+              </div>
+            ) : (
+              <img src={postDetail.imageUrl} alt="게시물이미지" />
+            )}
+          </div>
+
+          <div className={styles.contentSection}>
+            <p>{postDetail.content}</p>
+          </div>
+
           <button
-            key={i + 1}
-            className={currentPage === i + 1 ? styles.active : ""}
-            onClick={() => handlePaginationClick(i + 1)}
+            type="submit"
+            className={styles.commentButton}
+            onClick={handleCommentButtonClick}
           >
-            {i + 1}
+            댓글 등록하기
           </button>
-        ))}
-        <button
-          disabled={currentPage === totalPages}
-          onClick={() => handlePaginationClick(currentPage + 1)}
-        >
-          &raquo;
-        </button>
-      </div>
 
-      {showCommentModal && (
-        <CommentModal
-          closeModal={() => setShowCommentModal(false)}
-          onCommentSuccess={handleCommentSuccess} 
-        />
-      )}
-      {showModifyModal && (
-        <ModifyModal closeModal={() => setShowModifyModal(false)} />
-      )}
-      {showDeleteModal && (
-        <DeleteModal closeModal={() => setShowDeleteModal(false)} />
-      )}
+          <div className={styles.commentSection}>
+            <h3>댓글 {comments.length}</h3>
+          </div>
 
-      {showEditModal && (
-        <CommentEditModal
-          comment={commentToEdit}
-          closeModal={() => setShowEditModal(false)}
-          onSuccess={handleEditCommentSuccess}
-        />
-      )}
+          {errorMessage && <p className={styles.error}>{errorMessage}</p>}
 
-      {showCommentDeleteModal && (
-        <CommentDeleteModal
-          closeModal={() => setShowCommentDeleteModal(false)}
-          commentId={deletingCommentId}
-          onDelete={handleDeleteComment}
-        />
+          <div className={styles.commentList}>
+            {comments.map((comment) => (
+              <div key={comment.id} className={styles.commentItem}>
+                <div className={styles.commentInfo}>
+                  <span className={styles.commentAuthor}>
+                    {comment.nickname}
+                  </span>
+                  <span className={styles.commentDate}>
+                    {new Date(comment.createdAt).toLocaleString()}
+                  </span>
+                </div>
+                <div className={styles.commentContent}>
+                  <p className={styles.commentText}>{comment.content}</p>
+                  <div className={styles.commentActions}>
+                    <button
+                      className={styles.editButton}
+                      onClick={() => handleEditCommentClick(comment)}
+                    >
+                      <img src="src/assets/edit.svg" alt="수정" />
+                    </button>
+                    <button
+                      className={styles.deleteButton}
+                      onClick={() => handleDeleteCommentClick(comment.id)}
+                    >
+                      <img src="src/assets/delete.svg" alt="삭제" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className={styles.pagination}>
+            <button
+              disabled={currentPage === 1}
+              onClick={() => handlePaginationClick(currentPage - 1)}
+            >
+              &laquo;
+            </button>
+            {Array.from({ length: totalPages }, (_, i) => (
+              <button
+                key={i + 1}
+                className={currentPage === i + 1 ? styles.active : ""}
+                onClick={() => handlePaginationClick(i + 1)}
+              >
+                {i + 1}
+              </button>
+            ))}
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() => handlePaginationClick(currentPage + 1)}
+            >
+              &raquo;
+            </button>
+          </div>
+
+          {showCommentModal && (
+            <CommentModal
+              closeModal={() => setShowCommentModal(false)}
+              onSuccess={handleCommentSuccess}
+            />
+          )}
+
+          {showModifyModal && (
+            <ModifyModal
+              post={postDetail}
+              closeModal={() => setShowModifyModal(false)}
+            />
+          )}
+
+          {showDeleteModal && (
+            <DeleteModal closeModal={() => setShowDeleteModal(false)} />
+          )}
+
+          {showEditModal && (
+            <CommentEditModal
+              comment={commentToEdit}
+              closeModal={() => setShowEditModal(false)}
+              onSuccess={handleEditCommentSuccess}
+            />
+          )}
+
+          {showCommentDeleteModal && (
+            <CommentDeleteModal
+              closeModal={() => setShowCommentDeleteModal(false)}
+              onDelete={(password) =>
+                handleDeleteComment(deletingCommentId, password)
+              }
+            />
+          )}
+        </>
+      ) : (
+        <p>게시글을 불러오는 중입니다...</p>
       )}
     </div>
   );
